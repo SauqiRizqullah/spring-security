a758d2a006ee6868997d534cda01d7c8ba66b57d
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-10-01 18:43:41 +0700

    [ADD] Role-Based Authentication

--- src/main/java/com/basic/securitydemo/GreetingsController.java
+++ src/main/java/com/basic/securitydemo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.basic.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String helloAdmin() {
+        return "Hello, Admin!";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String helloUser() {
+        return "Hello, User!";
     }
 }
